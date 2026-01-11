2f3d798d3c08f4bbd769cdd73ff42bea700cab14
fernandahaiduk <78828184+fernandahaiduk@users.noreply.github.com>
2026-01-11 16:29:18 +0000

    Deploying to gh-pages from @ fernandahaiduk/fernandahaiduk.github.io@72267b9bfe95cbbbe21687151578e2d129721ecd 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,12 +9,12 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-blog",
-          title: "blog",
+  },{id: "nav-tramas",
+          title: "tramas",
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/blog/";
+            window.location.href = "/tramas/index.html";
           },
         },{id: "nav-publications",
           title: "publications",
